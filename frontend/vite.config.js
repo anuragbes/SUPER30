74a36dbaf99a82cb127f0784be5799a16b16143a
hraj-amr <hraj.amr@gmail.com>
2025-12-26 14:34:06 +0530

    vite config file updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,4 +11,17 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          charts: ["recharts"],
+          icons: ["lucide-react"],
+          http: ["axios"],
+        },
+      },
+    },
+  },
 })
